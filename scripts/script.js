22f7d0ea9ea3dbaf71774975e2c351df470cb127
DhanishMA <danishma2099pi@gmail.com>
2023-12-19 15:42:20 +0530

    3

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,10 @@
-// Get the link element by its id
-var button = document.getElementById("NowPlayingButton");
-
-// Add a click event listener to the button
-button.addEventListener("click", function() {
-  // Get the div element by its id
+function ShowNowPlaying() {
   var div = document.getElementById("NowPlayingDiv");
 
-  // Toggle the display property of the div
   if (div.style.display === "none") {
     div.style.display = "block";
   } else {
     div.style.display = "none";
   }
-});
+
+}
